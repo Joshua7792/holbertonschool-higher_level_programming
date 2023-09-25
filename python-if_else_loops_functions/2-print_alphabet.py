b9f11de2146ebe1b5d8fe9e8cfab31a6cece27c8
Joshua7792 <j.santiago39@hotmail.com>
2023-09-25 15:28:58 -0400

    prints the ASCII alphabet, in lowercase,

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -2,7 +2,7 @@
 # Start with the ASCII value of 'a'
 ascii_value = ord('a')
 
-# Iterate through the lowercase alphabet characters and print them without a new line
+# Iterate through the alphabet characters and print them without a new line
 for _ in range(26):
     print(chr(ascii_value), end="")
     ascii_value += 1
