202c113008049e4e46823db2f5c6236400bffdd0
Joshua7792 <j.santiago39@hotmail.com>
2023-09-25 15:27:46 -0400

    task 1

---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Start with the ASCII value of 'a'
+ascii_value = ord('a')
+
+# Iterate through the lowercase alphabet characters and print them without a new line
+for _ in range(26):
+    print(chr(ascii_value), end="")
+    ascii_value += 1
+
+# Print a new line at the end to separate the output
+print()
