8120039761f7b4934c9bff8ff94c5c3abd82b5bf
Joshua7792 <j.santiago39@hotmail.com>
2023-09-26 14:19:58 -0400

    fix to  string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for char in str:
         if 'a' <= char <= 'z':
             # Convert lowercase to uppercase using ASCII values
-            uppercase_char = chr(ord(char) - 32)
-        else:
-            uppercase_char = char  # Keep non-lowercase characters as they are
-        print(uppercase_char, end="")
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print()  # Print a newline character to end the output
