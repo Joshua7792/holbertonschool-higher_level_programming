2c418639a004614170ceb21b4209dc1c3bb470bf
Joshua7792 <j.santiago39@hotmail.com>
2023-09-26 13:40:03 -0400

    prints all possible combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # Use two loops to generate combinations of two digits
-for first_digit in range(0, 9):
+for first_digit in range(0, 10):
     for second_digit in range(first_digit + 1, 10):
-        # Print the combination with a comma and space
-        print("{:02d}, ".format(first_digit * 10 + second_digit), end="")
-    else:
-        # print the last combination without a comma and space
-        print("{:02d}".format(first_digit * 10 + second_digit), end="")
-
+        # Check if it's the last combination and print accordingly
+        if first_digit == 8 and second_digit == 9:
+            print("{:02d}".format(first_digit * 10 + second_digit), end="")
+        else:
+            print("{:02d}, ".format(first_digit * 10 + second_digit), end="")
 # Print a newline character to end the output
 print()
