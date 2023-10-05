d014bbf9c211870460150405b3992014e4f82906
Joshua7792 <j.santiago39@hotmail.com>
2023-10-05 15:06:14 -0400

    replaces or adds key/value in a dictionary.

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {key: value}
-    a_dictionary.update(new_dict)
-    return a_dictionary
+    new_dict = {key: value}  # Create a new dictionary with the key/value pair to update or add
+    a_dictionary.update(new_dict)  # Use the update() method to update the input dictionary
+    return a_dictionary  # Return the updated dictionary
