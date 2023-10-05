cd735d354126f9633228dcfd881add136fc07525
Joshua7792 <j.santiago39@hotmail.com>
2023-10-05 16:54:42 -0400

    fixed for checkers

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
+    if a_dictionary:
+        best_key = max(a_dictionary, key=a_dictionary.get)
+        return best_key
     else:
-        return(max(a_dictionary, key=a_dictionary.get))
+        return None
