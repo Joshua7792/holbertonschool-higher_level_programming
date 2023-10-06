a7865b5f1774c254516ed912741e290432766e51
Joshua7792 <j.santiago39@hotmail.com>
2023-10-06 15:49:07 -0400

    fixed for chekcers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # Iterate through each row in the matrix
     for row in matrix:
-        # Iterate through each element in the current row
         for i in range(len(row)):
-            # Check if the current element is not the last element in the row
-            if i != len(row) - 1:
-                # Print the current integer with formatting and add a space
-                print("{:d}".format(row[i]), end=" ")
-            else:
-                # If it's the last element in the row, just print the integer
-                print("{:d}".format(row[i]))
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+        print()
