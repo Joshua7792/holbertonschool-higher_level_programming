70f77ef67840b9bbbe5402912dadf5651b23e638
Joshua7792 <j.santiago39@hotmail.com>
2023-10-10 14:38:40 -0400

    fixed for checkers

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size=0):
         """Initialize sqr"""
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
-            self.Square__size = size
+            self._Square__size = size
