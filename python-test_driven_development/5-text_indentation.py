da2da594358bcdd605d1935e2e559ac9562ad067
Joshua7792 <j.santiago39@hotmail.com>
2023-10-13 15:01:57 -0400

    fixed for checkers

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Module to print a text with 2 new lines after each of these characters: ., ? and :"""
+"""Module to print a text with 2 new lines
+after each of these characters: ., ? and :"""
 
 
 def text_indentation(text):
-    """Function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """Function that prints a text with 2 new lines
+    after each of these characters: ., ? and :"""
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
         if text[i] in ".?:":
             print(text[i])
             print()
+        elif text[i] == " " and text[i - 1] in ".?:":
+            continue
         else:
             print(text[i], end="")
+    print()
