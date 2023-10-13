c492973676016283d5663c2e947c7f0754206c09
Joshua7792 <j.santiago39@hotmail.com>
2023-10-13 18:35:14 -0400

    fixed for checkers

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -88,5 +88,9 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """returns a new Rectangle instance"""
+        """Returns a new Rectangle instance as a square."""
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size < 0:
+            raise ValueError("width must be >= 0")
         return cls(size, size)
