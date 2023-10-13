ce9ef339bb325359e80a01176e7dbc5f866860f6
Joshua7792 <j.santiago39@hotmail.com>
2023-10-13 14:30:04 -0400

    fixed for checkers

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,12 +3,14 @@
 
 
 def print_square(size):
-    """Prints a square with the character #"""
+    """
+        Function that print a square with '#'
+        size - is the size lenght of the square
+    """
     if type(size) is not int:
-        raise TypeError("size must be an integer")
+        raise TypeError('size must be an integer')
     if size < 0:
-        raise ValueError("size must be >= 0")
-    for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print("")
+        raise ValueError('size must be >= 0')
+
+    for _ in range(size):
+        print("#" * size)
