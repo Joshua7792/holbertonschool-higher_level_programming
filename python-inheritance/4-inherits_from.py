d5c0955142610a546a26624514bb029612f75659
Joshua7792 <j.santiago39@hotmail.com>
2023-10-17 15:00:36 -0400

    fixed for checkers

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Defines a function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class; otherwise False."""
+""" Defines a function that returns True if the object is an instance of"""
 
 
 def inherits_from(obj, a_class):
-    """ Returns True if the object is an instance of
-    a class that inherited (directly or indirectly)"""
+    """ Returns True if the object is an instance of"""
     if type(obj) is a_class or isinstance(obj, a_class):
         return True
     else:
