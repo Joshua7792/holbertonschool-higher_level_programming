8c3b1a730df8acb1b88645b92055f89a4bfe0f95
Joshua7792 <j.santiago39@hotmail.com>
2023-10-17 15:04:46 -0400

    return instance of the object

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,8 +3,12 @@
 
 
 def inherits_from(obj, a_class):
-    """ Returns True if the object is an instance of"""
+    """
+    returns True if the object is
+    and instance of a class that inherited from
+    the specified class; otherwise False
+    """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
-        False
+        return False
