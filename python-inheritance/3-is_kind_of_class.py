ff9c033bc4c56e3e82cf45db67a99776242d3ad7
Joshua7792 <j.santiago39@hotmail.com>
2023-10-17 14:53:51 -0400

    fixed for checkers

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,7 +3,8 @@
 
 
 def is_kind_of_class(obj, a_class):
-    if isinstance(obj, a_class):
+    """ Returns True if the object is an instance of"""
+    if type(obj) is a_class or isinstance(obj, a_class):
         return True
     else:
         return False
