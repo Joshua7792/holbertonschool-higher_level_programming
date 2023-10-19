2200b2ea0d901c95f05584220d4b0431fbf37db4
Joshua7792 <j.santiago39@hotmail.com>
2023-10-19 14:58:10 -0400

    retrieve a dic representation of student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,11 +10,17 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is list:
-            i = {}
-            for j in attrs:
-                if j in self.__dict__:
-                    i[j] = self.__dict__[j]
-            return i
+        """ Retrieves a dictionary representation
+        of the Student class """
+        if attrs is not None:
+            new_dict = {}
+            for i in attrs:
+                if str(i) == "first_name":
+                    new_dict[i] = self.first_name
+                elif i == "last_name":
+                    new_dict[i] = self.last_name
+                elif i == "age":
+                    new_dict[i] = self.age
+            return new_dict
         else:
             return self.__dict__
