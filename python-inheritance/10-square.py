aefa840701f401c56995e00c06540cc62d138241
Joshua7792 <j.santiago39@hotmail.com>
2023-10-19 12:09:38 -0400

    updated file

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,16 +2,17 @@
 """Square class inherited from Rectangle class"""
 
 
-rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(rectangle):
+class Square(Rectangle):
+    """ Square class"""
     def __init__(self, size):
-        """Square class inherited from Rectangle class"""
-        self.__size = size
-        super().integer_validator("size", size)
+        """Initializes values"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        """Square class inherited from Rectangle class"""
-        return self.__size * self.__size
+        """Calculates area"""
+        return (self.__size * self.__size)
