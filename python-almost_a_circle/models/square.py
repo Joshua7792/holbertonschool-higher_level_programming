c32d9b6f15264013c422557c448d4b80e6db0bd0
Joshua7792 <j.santiago39@hotmail.com>
2023-10-23 18:01:47 -0400

    returns dictionary representation of a Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -37,3 +37,12 @@ class Square(Rectangle):
         elif kwargc > 0:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Dictionary representation of a square"""
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
