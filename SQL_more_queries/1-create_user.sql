caad441c8d9c07c25dd91d5e88fc0358f17f34f6
Joshua7792 <j.santiago39@hotmail.com>
2023-11-21 22:29:05 -0400

    fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,7 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pass';GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+
+-- If the user does not exist, create it
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
