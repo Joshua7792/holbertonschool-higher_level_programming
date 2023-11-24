fb377f4608ea7998269eb36c2fa87c6aef1b962f
Joshua7792 <j.santiago39@hotmail.com>
2023-11-24 10:37:12 -0400

    fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states"""
+"""script that lists all states with a name starting with N (upper N)"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MYSQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    """Function that lists all states from database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], db=sys.argv[4])
 
 # Creating cursor object
     cur = db.cursor()
 
-# Executing MySql Query
+    # Executing MySql Query
     cur.execute("SELECT * FROM states WHERE name = '{}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+                 ORDER BY id".format(search))
 
-# Query Result & prints the result in rows
-for row in cur.fetchall():
-    if row[1][0] == 'N':
-        print(row)
+    # Obtaining Query Result & prints the result in rows
+    for row in cur.fetchall():
+        if row[1][0] == 'N':
+            print(row)
 
-# Clean Up
-db.close()
-cur.close()
+    # Clean Up
+    cur.close()
+    db.close()
