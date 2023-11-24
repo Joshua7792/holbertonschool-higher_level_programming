e72c47ee741834c071760242c1f98956b561a4f3
Joshua7792 <j.santiago39@hotmail.com>
2023-11-24 10:55:05 -0400

    testing

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,8 @@ import sys
 if __name__ == "__main__":
     """Function that lists all states from database hbtn_0e_0_usa"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], db=sys.argv[4])
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         db=sys.argv[4])
 
 # Creating cursor object
     cur = db.cursor()
