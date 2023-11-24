94a1682d9d1c3300003c5453ac42dd8ab7936cf0
Joshua7792 <j.santiago39@hotmail.com>
2023-11-24 11:02:42 -0400

    fixed for checkers

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,22 +5,21 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    """Function that lists all states from database hbtn_0e_0_usa"""
+    # Function that lists all states from database hbtn_0e_0_usa
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 
-# Creating cursor object
+    # Creating cursor object
     cur = db.cursor()
 
-    # Executing MySql Query
-    cur.execute("SELECT * FROM states WHERE name = '{}'\
-                 ORDER BY id".format(sys.argv[4]))
+    # Using format to create the SQL query with user input
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4])
+    cur.execute(query)
 
     # Obtaining Query Result & prints the result in rows
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
 
     # Clean Up
     cur.close()
