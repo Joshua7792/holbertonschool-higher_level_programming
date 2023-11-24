99895dd0bd85d3d3671a725774901a7876ef3d77
Joshua7792 <j.santiago39@hotmail.com>
2023-11-24 10:45:44 -0400

    fixed for checkers

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,15 +4,16 @@ This script lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa.
 """
 
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     """Function that lists all states from database hbtn_0e_0_usa"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], db=sys.argv[4])
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-# Creating cursor object
+    # Creating cursor object
     cur = db.cursor()
 
     # Executing MySql Query
