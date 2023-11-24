ce21008145b6cfdbf1f54ff1d67fc0401a3ba5d7
Joshua7792 <j.santiago39@hotmail.com>
2023-11-24 01:18:43 -0400

    correct indentation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,18 +7,18 @@ import sys
 if __name__ == "__main__":
     """Function that lists all states from database hbtn_0e_0_usa"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-# Create a cursor object
+    # Create a cursor object
     cursor = db.cursor()
 
-# Execute the query
+    # Execute the query
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# print rows in a list of lists
+    # print rows in a list of lists
     for row in cursor.fetchall():
         print(row)
 
-# Clean up
+    # Clean up
     cursor.close()
     db.close()
